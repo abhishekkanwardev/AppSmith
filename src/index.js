eebb6c8129429f2974a9930cb78d88b80746bff0
Priyankush <pksalo6@gmail.com>
2022-11-02 22:02:02 +0530

    login page dsign

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import m from "mithril";
 import Home from "./Home";
+import Login from "./Login";
 import WholeSale_Filter from "./WholeSale_Filter";
 
 m.route(document.body, "/", {
-  "/": Home,
+  "/": Login,
   "/WholeSale_Filter": WholeSale_Filter,
+  "/dashboard": Home,
 });
